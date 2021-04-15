a12f73d709862bb7b741ae4e1f3cef3b9941b9e5
Allen Liao <namo50095@gmail.com>
2021-04-15 17:21:28 +0800

    updated

--- homeworks/week1/github.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for query in name bio location blog
-do
-	curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/$1 | grep $query | cut -d '"' -f 4
-done
-
--- homeworks/week1/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-num=1
-while [ $num -le $1 ]
-do
-	touch $num.js
-	num=`expr $num + 1`
-done
