25ae8e13a09e1ac2bd8b1b34802ead499dc3443e
Allen Liao <namo50095@gmail.com>
2021-04-16 11:55:50 +0800

    finished hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,19 @@
 function join(arr, concatStr) {
-  
+  let str = "";
+  arr.forEach((el, idx) => {
+    str += el;
+    if (idx !== arr.length - 1) str += concatStr;
+  });
+  return str;
 }
 
 function repeat(str, times) {
-  
+  let newStr = "";
+  for (let i = 0; i < times; i++) {
+    newStr += str;
+  }
+  return newStr;
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join([1, 2, 3], ""));
+console.log(repeat("yoyo", 2));
