585ec5c4229a8fc403bb841a381a1b0db1ba9557
Allen Liao <namo50095@gmail.com>
2021-04-16 10:39:37 +0800

    finished hw1

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,11 @@
 function printStars(n) {
-
+  if (n < 1 || n > 30) {
+    console.log("n 需在 1-30 之間");
+  } else {
+    for (let i = 1; i <= n; i++) {
+      i < 10 ? console.log(i, " *") : console.log(i, "*");
+    }
+  }
 }
 
-printStars(5)
+printStars(0);
