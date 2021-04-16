ff643a35d07c3e7a201a7b34eb1027c175e95260
Allen Liao <namo50095@gmail.com>
2021-04-16 08:49:41 +0800

    silent mode curl

--- homeworks/week1/github.sh
+++ homeworks/week1/github.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for query in name bio location blog
 do
-	curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/$1 | grep $query | cut -d '"' -f 4
+	curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/$1 | grep $query | cut -d '"' -f 4
 done
