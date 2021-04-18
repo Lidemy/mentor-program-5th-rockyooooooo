ecf4d2c5b279c90910dd01d18835a5e634be2f76
Allen Liao <namo50095@gmail.com>
2021-04-18 15:09:30 +0800

    fixed hw1 and super-challenge

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,11 +1,7 @@
 function printStars(n) {
-  if (n < 1 || n > 30) {
-    console.log("n 需在 1-30 之間");
-  } else {
-    for (let i = 1; i <= n; i++) {
-      i < 10 ? console.log(i, " *") : console.log(i, "*");
-    }
+  for (let i = 1; i <= n; i++) {
+    console.log('*')
   }
 }
 
-printStars(0);
+printStars(5)
--- homeworks/week2/super-challenge.js
+++ homeworks/week2/super-challenge.js
@@ -1,20 +1,20 @@
 function add(a, b) {
   // base case
-  if (a == 0 && b == 0) {
-    return "0";
-  } else if ((a == 1 && b == 0) || (a == 0 && b == 1)) {
-    return "1";
-  } else if (a == 1 && b == 1) {
-    return "10";
+  if (a === 0 && b === 0) {
+    return '0'
+  } else if ((a === 1 && b === 0) || (a === 0 && b === 1)) {
+    return '1'
+  } else if (a === 1 && b === 1) {
+    return '10'
   }
   // recursive case
   if (a & 1 && b & 1) {
-    return add(a >> 1, parseInt(add(b >> 1, 1), 2)) + "0";
+    return `${add(a >> 1, parseInt(add(b >> 1, 1), 2))}0`
   } else if (a & 1 || b & 1) {
-    return add(a >> 1, b >> 1) + "1";
+    return `${add(a >> 1, b >> 1)}1`
   } else if (~(a & 1) && ~(b & 1)) {
-    return add(a >> 1, b >> 1) + "0";
+    return `${add(a >> 1, b >> 1)}0`
   }
 }
 
-console.log(add(5, 3));
+console.log(parseInt(add(12, 45), 2))
