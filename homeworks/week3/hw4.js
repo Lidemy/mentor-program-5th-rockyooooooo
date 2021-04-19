5d6387b025b8c66f6fe7fe416a2a897cf2da8871
Allen Liao <namo50095@gmail.com>
2021-04-19 15:06:02 +0800

    finished hw4

---
+++ homeworks/week3/hw4.js
@@ -0,0 +1,34 @@
+function Palindrome(str) {
+  let i = 0
+  let j = str.length - 1
+
+  while (i < str.length && j >= 0) {
+    if (str[i] !== str[j]) return console.log('False')
+    i += 1
+    j -= 1
+  }
+  return console.log('True')
+}
+
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+const lines = []
+
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', (line) => {
+  lines.push(line)
+})
+
+// 輸入結束，開始針對 lines 做處理
+rl.on('close', () => {
+  solve(lines)
+})
+
+// 上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
+function solve(lines) {
+  Palindrome(lines[0])
+}
