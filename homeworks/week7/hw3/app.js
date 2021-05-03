3252fc89c98390447dc28b63858598ae2448545c
Allen Liao <namo50095@gmail.com>
2021-05-03 20:04:50 +0800

    hw3 finished basic functions

---
+++ homeworks/week7/hw3/app.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('.todo__form')
+const input = document.querySelector('.todo__input')
+const listContainer = document.querySelector('.list__container')
+
+function addTodo(e) {
+  e.preventDefault()
+  if (!input.value) return
+
+  const li = document.createElement('li')
+  li.innerHTML = `
+    <p>${input.value}</p>
+    <span class="material-icons close">close</span>
+    `
+  li.classList.add('list__item')
+  listContainer.appendChild(li)
+
+  input.value = ''
+}
+
+function removeTodo(e) {
+  e.preventDefault()
+
+  if (e.target.classList.contains('list__item')) {
+    listContainer.removeChild(e.target)
+  }
+
+  if (e.target.parentNode.classList.contains('list__item')) {
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode)
+  }
+}
+
+function toggleTodo(e) {
+  if (e.target.classList.contains('list__item')) {
+    e.target.classList.toggle('finished')
+  }
+
+  if (e.target.parentNode.classList.contains('list__item')) {
+    e.target.parentNode.classList.toggle('finished')
+  }
+}
+
+form.addEventListener('submit', addTodo)
+listContainer.addEventListener('contextmenu', removeTodo)
+listContainer.addEventListener('click', toggleTodo)
