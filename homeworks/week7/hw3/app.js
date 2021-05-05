0f662bc7bdb3a63428fc3c056d2031a6bda46466
Allen Liao <namo50095@gmail.com>
2021-05-05 22:22:54 +0800

    week7-hw3 added edit-function

--- homeworks/week7/hw3/app.js
+++ homeworks/week7/hw3/app.js
@@ -9,7 +9,12 @@ function addTodo(e) {
   const li = document.createElement('li')
   li.innerHTML = `
     <p>${input.value}</p>
-    <span class="material-icons close">close</span>
+    <input class="list_text hide" type="text" value="${input.value}">
+    <div class="list__btns">
+      <span class="material-icons-outlined finish hide">check_circle_outline</span>
+      <span class="material-icons-outlined edit">edit</span>
+      <span class="material-icons close">close</span>
+    </div>
     `
   li.classList.add('list__item')
   listContainer.appendChild(li)
@@ -31,17 +36,44 @@ function removeTodoFromLeftClick(e) {
 
 function removeTodoFromCloseBtn(e) {
   if (e.target.classList.contains('close')) {
-    e.target.parentNode.parentNode.removeChild(e.target.parentNode)
+    e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode)
   }
 }
 
 function toggleTodo(e) {
-  if (e.target.classList.contains('list__item')) {
+  if (e.target.classList.contains('list__item') && !e.target.classList.contains('editMode')) {
     e.target.classList.toggle('finished')
   }
 
-  if (e.target.parentNode.classList.contains('list__item')) {
+  if (e.target.parentNode.classList.contains('list__item') && !e.target.parentNode.classList.contains('editMode')) {
     e.target.parentNode.classList.toggle('finished')
+  }
+}
+
+function editMode(e) {
+  if (e.target.classList.contains('edit')) {
+    e.target.parentNode.parentNode.classList.toggle('editMode')
+    e.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.classList.toggle('hide')
+    e.target.parentNode.previousSibling.previousSibling.classList.toggle('hide')
+    e.target.previousSibling.previousSibling.classList.toggle('hide')
+    e.target.classList.toggle('hide')
+  }
+}
+
+function finishedEdit(e) {
+  if (e.target.classList.contains('finish')) {
+    const listEl = e.target.parentNode.parentNode
+    const finishBtn = e.target
+    const editBtn = e.target.nextSibling.nextSibling
+    const inputEl = e.target.parentNode.previousSibling.previousSibling
+    const pEl = e.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling
+    listEl.classList.toggle('editMode')
+    pEl.classList.toggle('hide')
+    inputEl.classList.toggle('hide')
+    editBtn.classList.toggle('hide')
+    finishBtn.classList.toggle('hide')
+
+    pEl.innerText = inputEl.value
   }
 }
 
@@ -49,3 +81,5 @@ form.addEventListener('submit', addTodo)
 listContainer.addEventListener('click', toggleTodo)
 listContainer.addEventListener('contextmenu', removeTodoFromLeftClick)
 listContainer.addEventListener('click', removeTodoFromCloseBtn)
+listContainer.addEventListener('click', editMode)
+listContainer.addEventListener('click', finishedEdit)
