cc253c4bd62f46215b342ae3864ebfa0019f8cd5
Allen Liao <namo50095@gmail.com>
2021-05-17 16:22:47 +0800

    week9-hw1, hw2 finished

---
+++ homeworks/week9/hw1/index.php
@@ -0,0 +1,84 @@
+<?php
+  session_start();
+  require_once('conn.php');
+
+  $username = null;
+  // 檢查 session
+  if (!empty($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+  
+    // 取得 users 資料
+    $format = 'SELECT nickname FROM users WHERE username = "%s"';
+    $sql = sprintf($format, $username);
+    $result = $conn->query($sql);
+    if (!$result) {
+      die('資料獲取失敗<br>' . $conn->error);
+    }
+    $row = $result->fetch_assoc();
+    $nickname = $row['nickname'];
+  }
+
+  // 取得 comments 資料
+  $format = 'SELECT * FROM comments ORDER BY id DESC';
+  $sql = sprintf($format);
+  $result = $conn->query($sql);
+  if (!$result) {
+    die('資料獲取失敗<br>' . $conn->error);
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>留言板</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header class="warning">
+    <strong>本網頁只限於練習，所以忽略了資安問題，切勿把真實帳號密碼輸入</strong>
+  </header>
+  <main class="container">
+    <section>
+      <h1>留言板</h1>
+      <?php if ($username) { ?>
+        <h3>歡迎回來，<?php echo $nickname ?></h3>
+        <button class="btn"><a href="handle_logout.php">登出</a></button>
+        <form class="form" method="POST" action="handle_add_comment.php">
+          <textarea class="form_input content" name="content" cols="30" rows="5"></textarea>
+          <?php 
+            if (!empty($_GET['errCode'])) {
+              $errCode = $_GET['errCode'];
+              if ($errCode === '1') {
+                $errMsg = '請輸入留言內容';
+              }
+              echo '<h3 class="error">' . $errMsg . '</h3>';
+            }
+          ?>
+          <button class="btn">送出</button>
+        </form>
+      <?php } else { ?>
+        <h2 class="error">請登入發布留言</h2>
+        <button class="btn"><a href="register.php">註冊</a></button>
+        <button class="btn"><a href="login.php">登入</a></button>
+      <?php } ?>
+    </section>
+    <section>
+      <?php while($row = $result->fetch_assoc()) { ?>
+        <article class="comment">
+          <div class="comment_avatar"></div>
+          <div class="comment_body">
+            <div class="comment_info">
+              <span class="comment_nickname"><?php echo $row['nickname'] ?></span>
+              <span class="comment_time"><?php echo $row['created_at'] ?></span>
+            </div>
+            <div class="comment_content"><?php echo $row['content'] ?></div>
+          </div>
+        </article>
+      <?php } ?>
+    </section>
+  </main>
+</body>
+</html>
--- homeworks/week9/hw2.md
+++ homeworks/week9/hw2.md
@@ -1,12 +1,41 @@
 ## 資料庫欄位型態 VARCHAR 跟 TEXT 的差別是什麼
 
+TEXT：
+- 最大長度固定是 65535 個 characters，無法限制最大長度
+- 佔用 (n + 2) bytes 的空間，n 為字串長度
+- 不能完全是索引的一部份，須給定前綴索引長度（prefix index length）
+- 查詢速度較慢
+- 使用時機：
+  - 要存放的內容長度不確定
+  - 不需要每一列的索引
 
+VARCHAR：
+- 最大長度可變，最多到 65535 個 characters
+- 如果 characters < 256 個，佔用 (n + 1) bytes 空間；如果 characters >= 256 個，佔用 (n + 2) bytes 空間。n 為字串長度
+- 可以是 index 的一部分
+- 查詢速度較快
+- 使用時機：
+  - 明確知道要儲存的字串長度
+  - 需要每一列的索引
 
 ## Cookie 是什麼？在 HTTP 這一層要怎麼設定 Cookie，瀏覽器又是怎麼把 Cookie 帶去 Server 的？
 
+因為 HTTP 是一個無狀態協議（Stateless Protocol），所以對 Server 來說，Client 的每次請求都是不一樣的，Server 沒辦法分辨每次請求之間的關係
+既然如此，假設有一個網站有會員系統，那 Server 要怎麼分辨 Client 是不是處於登入狀態呢？這時候 Cookie 就出現了，Cookie 可以用來儲存小片段的資料
 
+Cookie 主要有三個目的：
+- Session 管理
+- 個人化設定
+- 紀錄並分析使用者行為
 
+當 Client 成功登入帳號，Server 可以透過 Set-Cookie 這個 response header 來讓瀏覽器保存起來
+當下次 Client 再次發送請求時，瀏覽器就會附上這個 Cookie
+這樣 Server 就可以根據 Cookie 來檢查 Client 是不是有登入了
+也可以用在使用者的個人化設定，或是 Server 要根據使用者的行為來投放廣告...等等
 
 ## 我們本週實作的會員系統，你能夠想到什麼潛在的問題嗎？
 
-
+- 使用者可以在留言輸入 HTML tags 或 JavaScript 語法，我有用 `htmlentities()` 來避免
+- 使用者註冊帳號時，可以在 input 裡加入 HTML tags 或 JavaScript 語法，我有做簡單的檢查來避免
+- 密碼為明文，如果資料庫遭到入侵，就可以拿到正確密碼，可以用對稱加密或非對稱加密來把密碼轉換成密文
+- 判斷登入身分是用 Cookie 裡面存的 username，把 Cookie 裡的 username 改掉就可以偽裝成別人來登入
