6f39cf4116cb2c491a3cde61858200dc622bfb4b
Allen Liao <namo50095@gmail.com>
2021-05-28 15:43:17 +0800

    week11-hw2 index.php structure

---
+++ homeworks/week11/hw2/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Allen's Blog</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <h2 class="header__logo">Allen's Blog</h2>
+    <nav class="header__navbar">
+      <a class="btn" href="#">文章列表</a>
+      <a class="btn" href="#">分類專區</a>
+      <a class="btn" href="#">關於我</a>
+    </nav>
+    <div class="header__control">
+      <a class="btn" href="#">管理後臺</a>
+      <a class="btn" href="#">登出</a>
+      <a class="btn" href="#">註冊</a>
+      <a class="btn" href="#">登入</a>
+    </div>
+  </header>
+  <section class="banner">
+    <div class="banner__container">
+      <h1 class="banner__title">存放技術之地</h1>
+      <p class="banner__description">Welcome to my blog</p>
+    </div>
+  </section>
+  <section class="posts">
+    <article class="post">
+      <div class="post__header">
+        <h3 class="post__title">嗨～歡迎來到程式新手村 feat. 胡斯的異想世界</h3>
+        <button class="post__edit btn">編輯</button>
+      </div>
+      <div class="post__info">
+        <span class="material-icons-outlined">watch_later</span>2020/07/01 10:15
+        <span class="material-icons-outlined">folder</span>歷史公告
+      </div>
+      <div class="post__content">
+        <p>羅曼·羅蘭在不經意間這樣說過</p>
+        <p>只有把抱怨環境的心情，化為上進的力量，才是成功的保證。</p>
+        <p>帶著這句話，我們還要更加慎重的審視這個問題： 在這種困難的抉擇下，本人思來想去，寢食難安。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。</p>
+        <p>郭沫若說過一句富有哲理的話，形成天才的決定因素應該是勤奮。這句話語雖然很短，但令我浮想聯翩。每個人都不得不面對這些問題。在面對這種問題時， 我認為， 生活中，若中午吃什麼出現了，我們就不得不考慮它出現了的事實。中午吃什麼，發生了會如何，不發生又會如何。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。我們都知道，只要有意義，那麼就必須慎重考慮。現在，解決中午吃什麼的問題，是非常非常重要的。所以， 那麼， 一般來講，我們都必須務必慎重的考慮考慮</p>
+      </div>
+      <button class="post__read-more btn">READ MORE</button>
+    </article>
+    <article class="post">
+      <div class="post__header">
+        <h3 class="post__title">嗨～歡迎來到程式新手村 feat. 胡斯的異想世界</h3>
+        <button class="post__edit btn">編輯</button>
+      </div>
+      <div class="post__info">
+        <span class="material-icons-outlined">watch_later</span>2020/07/01 10:15
+        <span class="material-icons-outlined">folder</span>歷史公告
+      </div>
+      <div class="post__content">
+        <p>羅曼·羅蘭在不經意間這樣說過</p>
+        <p>只有把抱怨環境的心情，化為上進的力量，才是成功的保證。</p>
+        <p>帶著這句話，我們還要更加慎重的審視這個問題： 在這種困難的抉擇下，本人思來想去，寢食難安。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。</p>
+        <p>郭沫若說過一句富有哲理的話，形成天才的決定因素應該是勤奮。這句話語雖然很短，但令我浮想聯翩。每個人都不得不面對這些問題。在面對這種問題時， 我認為， 生活中，若中午吃什麼出現了，我們就不得不考慮它出現了的事實。中午吃什麼，發生了會如何，不發生又會如何。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。我們都知道，只要有意義，那麼就必須慎重考慮。現在，解決中午吃什麼的問題，是非常非常重要的。所以， 那麼， 一般來講，我們都必須務必慎重的考慮考慮</p>
+      </div>
+      <button class="post__read-more btn">READ MORE</button>
+    </article>
+    <article class="post">
+      <div class="post__header">
+        <h3 class="post__title">嗨～歡迎來到程式新手村 feat. 胡斯的異想世界</h3>
+        <button class="post__edit btn">編輯</button>
+      </div>
+      <div class="post__info">
+        <span class="material-icons-outlined">watch_later</span>2020/07/01 10:15
+        <span class="material-icons-outlined">folder</span>歷史公告
+      </div>
+      <div class="post__content">
+        <p>羅曼·羅蘭在不經意間這樣說過</p>
+        <p>只有把抱怨環境的心情，化為上進的力量，才是成功的保證。</p>
+        <p>帶著這句話，我們還要更加慎重的審視這個問題： 在這種困難的抉擇下，本人思來想去，寢食難安。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。</p>
+        <p>郭沫若說過一句富有哲理的話，形成天才的決定因素應該是勤奮。這句話語雖然很短，但令我浮想聯翩。每個人都不得不面對這些問題。在面對這種問題時， 我認為， 生活中，若中午吃什麼出現了，我們就不得不考慮它出現了的事實。中午吃什麼，發生了會如何，不發生又會如何。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。我們都知道，只要有意義，那麼就必須慎重考慮。現在，解決中午吃什麼的問題，是非常非常重要的。所以， 那麼， 一般來講，我們都必須務必慎重的考慮考慮</p>
+      </div>
+      <button class="post__read-more btn">READ MORE</button>
+    </article>
+  </section>
+  <footer>Copyright © 2020 Who's Blog All Rights Reserved.</footer>
+</body>
+</html>
