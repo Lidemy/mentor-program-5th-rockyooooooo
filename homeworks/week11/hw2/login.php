e0dd4e6406b110b35738e8d57c3c49e63ce6ac31
Allen Liao <namo50095@gmail.com>
2021-05-31 22:12:13 +0800

    week11-hw2 added RWD and about_me.php

--- homeworks/week11/hw2/login.php
+++ homeworks/week11/hw2/login.php
@@ -19,6 +19,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Allen's Blog - Login</title>
   <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
 </head>
 <body>
   <?php include('./views/header.php') ?>
--- homeworks/week11/hw2/views/footer.php
+++ homeworks/week11/hw2/views/footer.php
@@ -1,1 +1,7 @@
 <footer>Copyright © 2021 Allen's Blog All Rights Reserved.</footer>
+<script>
+  const headerNav = document.querySelector('.header__nav')
+  document.querySelector('.hamburger').addEventListener('click', () => {
+    headerNav.classList.toggle('show')
+  })
+</script>
