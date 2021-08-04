7e8cc16403fd920dae9d4b49c83d55d6d1ea5da7
Allen Liao <namo50095@gmail.com>
2021-08-04 19:43:38 +0800

    finished week21-hw1

---
+++ homeworks/week21/hw1/my-app/src/components/TodoItem.js
@@ -0,0 +1,79 @@
+import { useRef } from 'react'
+import styled from 'styled-components'
+import Button from './Button'
+import colors from '../constants/colors'
+
+const TodoItemWrapper = styled.li`
+  position: relative;
+  max-width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  transition: background 300ms ease;
+
+  &:hover button {
+    opacity: 1;
+  }
+
+  ${(props) => !props.$isChecked && `&:hover {background: ${colors.bg02};}`}
+`
+
+const TodoCheckBox = styled.input`
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+`
+
+const TodoContent = styled.pre`
+  flex: 1;
+  overflow-wrap: anywhere;
+  font-size: 1.5rem;
+  color: ${colors.gray01};
+  padding: 0.25rem 0.5rem;
+  border: 1px solid transparent;
+  border-radius: 0;
+  cursor: default;
+  transition: color 300ms ease, border-color 300ms ease;
+
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid ${colors.gray01};
+    cursor: text;
+  }
+
+  ${(props) => props.$isChecked && 'text-decoration: line-through;'}
+  ${(props) => props.$isChecked && `color: ${colors.gray03};`}
+`
+
+const DeleteTodoButton = styled(Button)`
+  opacity: 0;
+  flex-shrink: 0;
+  transition: color 300ms ease, opacity 300ms ease;
+`
+
+export default function TodoItem({ todo, handleDeleteTodoButtonClick, handleTodoCheckBoxChange, handleTodoContentUpdate }) {
+  const todoContentRef = useRef()
+  return (
+    <TodoItemWrapper $isChecked={todo.isChecked}>
+      <TodoCheckBox
+        type="checkbox"
+        checked={todo.isChecked}
+        onChange={handleTodoCheckBoxChange(todo.id)}
+      />
+      <TodoContent
+        ref={todoContentRef}
+        $isChecked={todo.isChecked}
+        onBlur={() => handleTodoContentUpdate(todo.id)(todoContentRef.current.innerText)}
+        contentEditable
+      >
+        {todo.content}
+      </TodoContent>
+      <DeleteTodoButton onClick={handleDeleteTodoButtonClick(todo.id)}>
+        刪除
+      </DeleteTodoButton>
+    </TodoItemWrapper>
+  )
+}
